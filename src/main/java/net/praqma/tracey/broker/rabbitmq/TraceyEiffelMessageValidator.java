e469dfb4c0d84e9716108e8fa30d1fc1a4036450
Mads Nielsen <man@praqma.net>
2016-07-11 12:14:21 +0200

    Added helper methods for Parsing of JSON

--- src/main/java/net/praqma/tracey/broker/rabbitmq/TraceyEiffelMessageValidator.java
+++ src/main/java/net/praqma/tracey/broker/rabbitmq/TraceyEiffelMessageValidator.java
@@ -1,17 +1,32 @@
 package net.praqma.tracey.broker.rabbitmq;
 
 import com.google.protobuf.GeneratedMessage;
+import net.praqma.tracey.protocol.eiffel.events.EiffelSourceChangeCreatedEventOuterClass;
 import org.json.JSONObject;
 
+//TODO: This should be more Object oriented
 public class TraceyEiffelMessageValidator {
 
     public static boolean isA(Class<? extends GeneratedMessage> ext, String payload) {
         JSONObject obj = new JSONObject(payload);
-        return obj.getJSONObject("meta") != null && obj.getJSONObject("meta").getString("type").equalsIgnoreCase(ext.getSimpleName().toLowerCase());
+        return obj.getJSONObject("meta") != null && obj.getJSONObject("meta").getString("type").equalsIgnoreCase(ext.getSimpleName());
+    }
+
+    public static boolean isA(String typename, String payload) {
+        JSONObject obj = new JSONObject(payload);
+        return obj.getJSONObject("meta") != null && obj.getJSONObject("meta").getString("type").equalsIgnoreCase(typename);
     }
 
     public static boolean isEiffel(String payload) {
         JSONObject obj = new JSONObject(payload);
         return obj.getJSONObject("meta") != null && obj.getJSONObject("meta").getString("type") != null;
+    }
+
+    public static JSONObject getGitIdentifier(String payload) {
+        if (!isA(EiffelSourceChangeCreatedEventOuterClass.EiffelSourceChangeCreatedEvent.class, payload)) {
+            return null;
+        }
+        JSONObject obj = new JSONObject(payload).getJSONObject("gitIdentifier");
+        return obj;
     }
 }
